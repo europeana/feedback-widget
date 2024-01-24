24862096553186eeb9f4b8bfcea999a14461f45d
Richard Doe <richard.doe@rwdit.net>
2024-01-24 14:18:16 +0000

    chore: update i18n component call

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { computed, ref } from 'vue'
 
-import CancelCircleIcon from '@europeana/style/img/icons/cancel_circle.svg'
-import CheckCircleIcon from '@europeana/style/img/icons/check_circle.svg'
-import ExternalLinkIcon from '@europeana/style/img/icons/external-link.svg'
+import CancelCircleIcon from  '@europeana/style/img/icons/cancel_circle.svg'
+import CheckCircleIcon from  '@europeana/style/img/icons/check_circle.svg'
+import ExternalLinkIcon from  '@europeana/style/img/icons/external-link.svg'
 
 const currentStep = ref(1)
 const email = ref('')
@@ -13,26 +13,20 @@ const feedbackTextarea = ref(null)
 const requestSuccess = ref(null)
 const sending = ref(false)
 
-const disableNextButton = computed(
-  () => (currentStep.value === 1 && feedback.value === '') || sending.value
-)
-const disableSendButton = computed(
-  () => (currentStep.value === 2 && email.value === '') || sending.value
-)
+const disableNextButton = computed(() => ((currentStep.value === 1) && (feedback.value === '')) || sending.value)
+const disableSendButton = computed(() => ((currentStep.value === 2) && (email.value === '')) || sending.value)
 const disableSkipButton = computed(() => sending.value)
-const showCancelButton = computed(() => currentStep.value < 3 || !requestSuccess.value)
+const showCancelButton = computed(() => (currentStep.value < 3) || !requestSuccess.value)
 const showCloseButton = computed(() => !showCancelButton.value)
 const showNextButton = computed(() => currentStep.value < 2)
-const showSendButton = computed(
-  () => currentStep.value === 2 || (currentStep.value === 3 && !requestSuccess.value)
-)
+const showSendButton = computed(() => currentStep.value === 2 || ((currentStep.value === 3) && !requestSuccess.value))
 const showSkipButton = computed(() => currentStep.value === 2)
 
 const localePath = (path) => `/en${path}`
 
 const wordLength = (text) => text?.trim()?.match(/\w+/g)?.length || 0
 
-const goToStep = (step) => (currentStep.value = step)
+const goToStep = (step) => currentStep.value = step
 
 const handleInputFeedback = () => {
   if (wordLength(feedback.value) < 5) {
@@ -72,7 +66,7 @@ const sendFeedback = () => {
     })
 }
 
-const submitForm = async () => {
+const submitForm = async() => {
   if (currentStep.value > 1) {
     await sendFeedback()
   }
@@ -83,7 +77,11 @@ const submitForm = async () => {
 </script>
 
 <template>
-  <form class="europeana-feedback-form" data-qa="feedback widget form" @submit.prevent="submitForm">
+  <form
+    class="europeana-feedback-form"
+    data-qa="feedback widget form"
+    @submit.prevent="submitForm"
+  >
     <div class="d-flex flex-wrap">
       <div class="form-fields">
         <div v-if="currentStep === 1">
@@ -100,11 +98,7 @@ const submitForm = async () => {
             aria-describedby="input-live-feedback"
             @input="handleInputFeedback"
           />
-          <div
-            class="b-form-invalid-feedback"
-            id="input-live-feedback"
-            data-qa="feedback message invalid"
-          >
+          <div class="b-form-invalid-feedback" id="input-live-feedback" data-qa="feedback message invalid">
             {{ $t('validFeedback') }}
           </div>
         </div>
@@ -118,28 +112,28 @@ const submitForm = async () => {
             :placeholder="$t('form.placeholders.email')"
             aria-describedby="input-live-feedback"
           />
-          <div
-            class="b-form-invalid-feedback"
-            id="input-live-feedback"
-            data-qa="feedback email invalid"
-          >
+          <div class="b-form-invalid-feedback" id="input-live-feedback" data-qa="feedback email invalid">
             {{ $t('validEmail') }}
           </div>
           <div class="b-form-text" id="input-live-help">
             <p class="mb-0">
               {{ $t('emailOptional') }}
-              <!-- <i18n path="feedback.policies" tag="span">
+              <i18n-t keypath="policies" tag="span">
                 <a :href="localePath('/rights')" target="_blank">
                   {{ $t('termsOfService') }}
                 </a>
                 <a :href="localePath('/rights/privacy-policy')" target="_blank">
                   {{ $t('privacyPolicy') }}
                 </a>
-              </i18n> -->
+              </i18n-t>
             </p>
           </div>
         </div>
-        <div v-if="currentStep == 3" id="step3" class="feedback-success d-flex align-items-center">
+        <div
+          v-if="currentStep == 3"
+          id="step3"
+          class="feedback-success d-flex align-items-center"
+        >
           <span v-if="requestSuccess">
             <CheckCircleIcon class="icon-check-circle mr-3" />
             <p class="mb-0">{{ $t('success') }}</p>
@@ -171,7 +165,7 @@ const submitForm = async () => {
             :disabled="disableSkipButton"
             @click="skipEmail"
           >
-            {{ $t('skipSend') }}</button
+            {{ $t('skipSend') }} </button
           ><!-- This comment removes white space
           --><button
             v-if="showNextButton"
