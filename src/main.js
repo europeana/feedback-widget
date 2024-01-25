005a9df6dc58b9ea982b333f56031a004e8f5f62
Richard Doe <richard.doe@rwdit.net>
2024-01-25 11:35:04 +0000

    chore: add dev backend

--- src/main.js
+++ src/main.js
@@ -7,15 +7,29 @@ import * as messages from './locales/index.js'
 const mountSelector = '#europeana-feedback-widget'
 const mountElement = document.querySelector(mountSelector)
 
+const configDefaults = {
+  apiUrl: 'https://www.europeana.eu/_api/jira-service-desk/feedback',
+  locale: 'en'
+}
+const config = Object.keys(configDefaults).reduce((memo, cKey) => {
+  if (Object.hasOwn(mountElement.dataset, cKey)) {
+    memo[cKey] = mountElement.dataset[cKey]
+  } else {
+    memo[cKey] = configDefaults[cKey]
+  }
+  return memo
+}, {})
+
 const i18n = createI18n({
   legacy: false,
-  locale: mountElement.dataset.locale,
-  fallbackLocale: 'en',
+  locale: config.locale,
+  fallbackLocale: configDefaults.locale,
   silentFallbackWarn: true,
   globalInjection: true,
   messages
 })
 
 createApp(App, { ...mountElement.dataset })
+  .provide('config', config)
   .use(i18n)
   .mount(mountSelector)
