d46995545cdfbff194ce76279445627b88788d53
Richard Doe <richard.doe@rwdit.net>
2024-01-29 10:01:06 +0000

    refactor: extract config

--- src/main.js
+++ src/main.js
@@ -2,34 +2,19 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
+import { createConfig, defaults } from './config.js'
 import * as messages from './locales/index.js'
 
 const mountSelector = '#europeana-feedback-widget'
-const mountElement = document.querySelector(mountSelector)
-
-const configDefaults = {
-  apiUrl: 'https://www.europeana.eu/_api/jira-service-desk/feedback',
-  locale: 'en'
-}
-const config = Object.keys(configDefaults).reduce((memo, cKey) => {
-  if (Object.hasOwn(mountElement.dataset, cKey)) {
-    memo[cKey] = mountElement.dataset[cKey]
-  } else {
-    memo[cKey] = configDefaults[cKey]
-  }
-  return memo
-}, {})
+const config = createConfig(mountSelector)
 
 const i18n = createI18n({
   legacy: false,
   locale: config.locale,
-  fallbackLocale: configDefaults.locale,
+  fallbackLocale: defaults.locale,
   silentFallbackWarn: true,
   globalInjection: true,
   messages
 })
 
-createApp(App, { ...mountElement.dataset })
-  .provide('config', config)
-  .use(i18n)
-  .mount(mountSelector)
+createApp(App).provide('config', config).use(i18n).mount(mountSelector)
