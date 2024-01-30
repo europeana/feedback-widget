d76171a94eb2e671a26a19d1c7416b72da65d715
Leonie Peters <leoniepeters@gmail.com>
2024-01-30 17:40:33 +0100

    Add test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -9,7 +9,11 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      setupFiles: ['./vitest.setup.js']
+      setupFiles: ['./vitest.setup.js'],
+      coverage: {
+        enabled: true,
+        provider: 'v8'
+      },
     }
   })
 )
