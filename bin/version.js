b03d3e00276f3ff83524b06bfa09312400dfbe2e
Richard Doe <richard.doe@rwdit.net>
2024-01-30 14:16:12 +0000

    chore: fix preversion, rm console logs

--- bin/version.js
+++ bin/version.js
@@ -1,23 +1,17 @@
 import fs from 'fs'
 import ssri from 'ssri'
 
-import pkg from '../package.json' assert { type: 'json' }
-
-console.log('pkg version', pkg.version)
-
 const readmeUrl = new URL('../README.md', import.meta.url)
 
 let readme = fs.readFileSync(readmeUrl, { encoding: 'utf8' })
 
+import pkg from '../package.json' assert { type: 'json' }
 readme = readme.replace(/@europeana\/feedback-widget(@.+)?\/dist\//g, `@europeana/feedback-widget@${pkg.version}/dist/`)
 
 const distScriptUrl = new URL('../dist/europeana-feedback-widget.js', import.meta.url)
 const distScriptIntegrity = ssri.fromData(fs.readFileSync(distScriptUrl), {
   algorithms: ['sha384']
 })
-console.log('distScriptIntegrity', distScriptIntegrity)
 readme = readme.replace(/integrity="sha384-[^"]*"/g, `integrity="${distScriptIntegrity.sha384.toString()}"`)
 
 fs.writeFileSync(readmeUrl, readme)
-
-process.exit(1)
