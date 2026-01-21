51f132f09201f9ccdc8235ab21325b0b4703e9c8
Richard Doe <richard.doe@rwdit.net>
2026-01-21 15:33:08 +0000

    build: create distinct app/component builds

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL('./src/main.js', import.meta.url)),
-      name: 'EuropeanaFeedbackWidget',
-      fileName: 'europeana-feedback-widget',
-      formats: ['es']
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: 'europeana-feedback-widget.[ext]'
-      }
-    }
-  },
   css: {
     preprocessorOptions: {
       scss: {
